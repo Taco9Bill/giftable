145d1e1b881ea3e600bd9720544183123c2c7cf7
taco9bill <taco9bill@gmail.com>
2020-05-17 01:30:31 -0400

    Updates

---
+++ precache-manifest.8f82379092e6e7621d920154d2ec7f73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ddc15ccc410b5038d5550bfdcec833d",
+    "url": "/giftable/index.html"
+  },
+  {
+    "revision": "1ee9a2ae3e25eb2f0d43",
+    "url": "/giftable/static/css/main.08b8c344.chunk.css"
+  },
+  {
+    "revision": "8795be7c085877fdc4fc",
+    "url": "/giftable/static/js/2.a7dfe936.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/giftable/static/js/2.a7dfe936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ee9a2ae3e25eb2f0d43",
+    "url": "/giftable/static/js/main.2ac2a7e3.chunk.js"
+  },
+  {
+    "revision": "16cd4948b815b567e77b",
+    "url": "/giftable/static/js/runtime-main.7ab7017d.js"
+  },
+  {
+    "revision": "877c0cd7fbf467f4c80bd5ecff9bb0f7",
+    "url": "/giftable/static/media/atsumare.877c0cd7.ttf"
+  }
+]);

37a5d7cb38961d31ab38c0343c79533a6a75eaa3
taco9bill <taco9bill@gmail.com>
2020-05-24 22:49:10 -0400

    Giftable wire up and initialization

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Category } from 'modules/entity.js'
+import { makeCatalog, makeRoster } from 'modules/dataloader.js'
+import { RecipientList, RecipientLookup } from 'components/recipients.js'
+import { GiftLookup, GiftSearchList } from 'components/gifts.js'
+import './normalize.css'
 import './App.css';
+import rawVillagers from './ds/villagers.json'
+import rawItems from './ds/items.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const inclCats = [
+    Category.Tops, Category.Bottoms, Category.DressUp, Category.Headwear,
+    Category.Socks, Category.Shoes, Category.Accessories, Category.Bags
+];
+const roster = makeRoster(rawVillagers);
+const catalog = makeCatalog(rawItems, inclCats);
+
+class Giftable extends React.Component {
+    constructor(props) {
+        super(props);
+        const selected = JSON.parse(localStorage.getItem('recipients') || null);
+        this.state = {
+          giftName: '',
+          recipientName: '',
+          recipientList: (selected && roster.findByIds(selected)) || []
+        };
+        this.onChangeGiftName = this.onChangeGiftName.bind(this);
+        this.onChangeRecipientName = this.onChangeRecipientName.bind(this);
+        this.toggleRecipientCallback = this.toggleRecipientCallback.bind(this);
+        this.onClickSearchResult = this.onClickSearchResult.bind(this);
+    }
+    setGiftName(value){
+        this.setState({giftName: value});
+    }
+    setRecipientName(value){
+        this.setState({recipientName: value});
+    }
+    setRecipientList(value){
+        this.setState({recipientList: value});
+    }
+    onChangeRecipientName(e) {
+        if (e.currentTarget) {
+            this.setRecipientName(e.currentTarget.value);
+        }
+    }
+    onChangeGiftName(e) {
+        if (e.currentTarget) {
+            this.setGiftName(e.currentTarget.value);
+        }
+    }
+    onClickSearchResult(item){
+        this.setGiftName(item.name);
+    }
+    toggleRecipientCallback(recipient){
+        let list = this.state.recipientList.slice();
+        const pos = list.indexOf(recipient);
+        if(pos !== -1){
+            list.splice(pos,1);
+            localStorage.setItem('recipients', JSON.stringify(list.map(r => r.id)));
+            this.setRecipientList(list);
+        }
+        else{
+            list.push(recipient);
+            list.sort((a,b) => a.name > b.name);
+            localStorage.setItem('recipients', JSON.stringify(list.map(r => r.id)));
+            this.setState({
+                recipientList: list,
+                recipientName: ''
+            });
+        }
+    }
+    render() {
+        return (
+          <div className="mori">
+            <div className="residents">
+            <div className="mori-search">
+                <RecipientLookup 
+                    value={this.state.recipientName}
+                    onChange={this.onChangeRecipientName}
+                />
+            </div>
+            <RecipientList
+                members={roster}
+                selected={this.state.recipientList}
+                recipientName={this.state.recipientName}
+                toggleCallback={this.toggleRecipientCallback}
+            />
+            </div>
+            <div className="inventory">
+            <div className="mori-search">
+                <GiftLookup
+                    value={this.state.giftName}
+                    onChange={this.onChangeGiftName}
+                />
+            </div>
+            <GiftSearchList
+                items={catalog}
+                giftName={this.state.giftName}
+                itemSelectCallback={this.onClickSearchResult}
+                recipients={this.state.recipientList}
+            />
+            </div>
+
+          </div>
+        );
+    }
 }
 
-export default App;
+export default Giftable;

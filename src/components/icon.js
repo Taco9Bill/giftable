72150677cae852e8088f56cc143656b0d6af0d43
taco9bill <taco9bill@gmail.com>
2020-05-28 23:22:04 -0400

    Added item color icons

--- src/components/icon.js
+++ src/components/icon.js
@@ -3,10 +3,11 @@ import Present from './img/Present.png';
 import BlueRibbonPresent from './img/Present2.png';
 import BellBag from './img/MoneyBag069.png';
 import GiftWrap from './img/WPaperYellow.png'
+import './icon.css';
 
 class GiftBoxIcon extends React.Component {
 	render(){
-		const desc = 'May receive gift or bells in return';
+		const desc = 'Will receive gift or bells in return';
 		return <img src={Present} alt='gift box badge' title={desc} />;
 	}
 }
@@ -30,4 +31,15 @@ class GiftWrapIcon extends React.Component {
 		return <img src={GiftWrap} alt="gift wrap badge" title={desc} />;
 	}
 }
-export {BellBagIcon, GiftBoxIcon, AltGiftBoxIcon, GiftWrapIcon};
+
+class ItemColorsIcon extends React.Component {
+	render(){
+		const [color1, color2] = this.props.colors;
+		const titleAttr = `${color1} / ${color2}`;
+		const classes = `colors color1-${color1.replace(/\s+/g, '')} color2-${color2.replace(/\s+/g, '')}`;
+		const icon = <div className={classes} title={titleAttr}>&nbsp;</div>;
+		return icon;
+	}
+}
+
+export {BellBagIcon, GiftBoxIcon, ItemColorsIcon, AltGiftBoxIcon, GiftWrapIcon};
